15917a45cbcc0a01d5eab0a39dda0e5379c9d42d
Daniel Dyba <daniel.dyba@gmail.com>
2011-10-15 11:16:48 -0700

    Added 'cdr' and 'car' tests
    Ignoring *.swp Vim generated files

--- syntax-ch3/syntax.lisp
+++ syntax-ch3/syntax.lisp
@@ -2,10 +2,21 @@
 (use-package :lisp-unit)
 
 (define-test understanding-cons
-  (assert-equal '(hello) (cons 'hello nil))
-  (assert-equal '(hello) (cons 'hello ()))
-  (assert-equal '(hello world) (cons 'hello (cons 'world nil)))
+  (assert-equal '(hello)          (cons 'hello nil))
+  (assert-equal '(hello)          (cons 'hello ()))
+  (assert-equal '(hello world)    (cons 'hello (cons 'world nil)))
   (assert-equal '(hello my world) (cons 'hello (cons 'my (cons 'world nil))))
+  )
+
+(define-test understanding-car
+  (assert-equal 'hello            (car (cons 'hello (cons 'world nil))))
+  (assert-equal 'world            (car '(world)))
+  (assert-equal 'hello            (car '(hello world)))
+  )
+
+(define-test understanding-cdr
+  (assert-equal '(world)          (cdr '(hello world)))
+  (assert-equal '(beef chicken)   (cdr '(goose beef chicken)))
   )
 
 (run-tests)

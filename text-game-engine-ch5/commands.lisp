1ad040c77354aa4c9f73ba95f0b0b8e56768e219
Daniel Dyba <daniel.dyba@gmail.com>
2011-10-25 11:57:37 -0700

    Add tests for describe-path and describe-paths functions

--- text-game-engine-ch5/commands.lisp
+++ text-game-engine-ch5/commands.lisp
@@ -9,3 +9,19 @@
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
+;;; describe-path <edge>
+;;;
+;;; This function returns a short sentence describing the 
+;;; next location available if you follow the <edge>.
+(defun describe-path (edge)
+  `(There is a ,(caddr edge) going ,(cadr edge) from here.))
+
+;;; describe-paths <location> <edges>
+;;;
+;;; This function returns a richer sentence describing all
+;;; <edges> leading to a new location from your current
+;;; <location>.
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
+  )
+
--- text-game-engine-ch5/test/describe_scenery_test.lisp
+++ text-game-engine-ch5/test/describe_scenery_test.lisp
@@ -5,10 +5,21 @@
                            (bathroom (I am in the bathroom.))
                            (kitchen (I am in the kitchen.))))
 
+(defparameter *house-edges* '((living-room (bathroom south door)
+                                           (kitchen west hall))
+                              (bathroom (living-room north door))
+                              (kitchen (living-room east hall))))
+
 (define-test describe-location-test
   (assert-equal '(I am in the living room.) (describe-location 'living-room *my-house*))
   (assert-equal '(I am in the bathroom.) (describe-location 'bathroom *my-house*))
   (assert-equal '(I am in the kitchen.) (describe-location 'kitchen *my-house*)))
+
+(define-test describe-path-test
+  (assert-equal '(There is a door going south from here.) (describe-path '(bathroom south door))))
+
+(define-test describe-paths-test
+  (assert-equal '(There is a door going south from here. There is a hall going west from here.) (describe-paths 'living-room *house-edges*)))
 
 (run-tests)
 

668cba8d459464c651cb9f23c8d96129bbcf2907
Daniel Dyba <daniel.dyba@gmail.com>
2015-09-17 23:00:05 -0700

    Removes nodes.lisp; completes chapter 5

--- text-game-engine-ch5/commands.lisp
+++ text-game-engine-ch5/commands.lisp
@@ -1,4 +1,26 @@
-(load "nodes.lisp")
+(defparameter *nodes* '((living-room (you are in the living-room.
+                            a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                            there is a well in front of you))
+                        (attic (you are in the attic.
+				there is a giant welding torch in the corner.))))
+
+(defparameter *edges* '((living-room
+			 (garden west door)
+			 (attic upstairs ladder))
+			(garden
+			 (living-room east door))
+			(attic
+			 (living-room downstairs ladder))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+				   (bucket living-room)
+				   (chain garden)
+				   (frog garden)))
+
+(defparameter *location* 'living-room)
 
 ;;; describe-location <location> <nodes>
 ;;;
@@ -22,8 +44,7 @@
 ;;; <edges> leading to a new location from your current
 ;;; <location>.
 (defun describe-paths (location edges)
-  (apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
-  )
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 ;;; objects-at <location> <objects> <object-location-mapping>
 ;;;
@@ -44,3 +65,32 @@
   (labels ((describe-object (obj)
               `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-object (objects-at loc objs obj-loc)))))
+
+;; Impure functions...
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+	  (describe-paths *location* *edges*)
+	  (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+		    (cdr (assoc *location* *edges*))
+		    :key #'cadr)))
+    (if next
+	(progn (setf *location* (car next))
+	       (look))
+	'(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+		 (objects-at *location* *objects* *object-locations*))
+	 (push (list object 'body) *object-locations*)
+	 `(you are now carrying the ,object))
+	(t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+;; TODO: drop an item from your inventory
+;; TODO: Set a limit on how many items you can carry
